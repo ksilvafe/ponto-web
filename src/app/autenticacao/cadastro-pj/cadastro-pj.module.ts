0d16600de8fa9d08cfa6d6660beeaeff652c26e4
kevinferreiradev <ferreirakevin325@gmail.com>
2022-03-25 15:53:09 -0300

    create cadastro pessoapj

--- src/app/autenticacao/cadastro-pj/cadastro-pj.module.ts
+++ src/app/autenticacao/cadastro-pj/cadastro-pj.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CadastrarPjComponent, CadastroPjComponent } from './components';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { CadastrarPjComponent, CadastroPjComponent } from './components';
 
 
 
@@ -15,7 +24,15 @@ import { FlexLayoutModule } from '@angular/flex-layout';
   imports: [
     CommonModule,
     RouterModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatListModule,
+    MatToolbarModule
   ]
 })
 export class CadastroPjModule { }
--- src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
+++ src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-cadastrar-pj',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarPjComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.gerarForm();
   }
 
+  gerarForm() {
+    this.form = this.fb.group({
+      nome: ['', [Validators.required, Validators.minLength(3), Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      cpf: ['', [Validators.required]],
+      razaoSocial: ['', [Validators.required, Validators.minLength(5)]],
+      cnpj: ['', [Validators.required]]
+    });
+  }
+
+  cadastrarPj() {
+    if (this.form.invalid) {
+      return;
+    }
+    alert(JSON.stringify(this.form.value));
+  }
 }
