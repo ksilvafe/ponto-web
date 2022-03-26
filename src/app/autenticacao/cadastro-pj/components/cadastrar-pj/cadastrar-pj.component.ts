68dae79d84807a32e395a09acf019a15f9dadbf9
kevinferreiradev <ferreirakevin325@gmail.com>
2022-03-26 00:35:10 -0300

    create validator cnpj

--- src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
+++ src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { CadastroPj } from '../../models';
-import { CpfValidator } from 'src/app/shared/validators';
+import { CpfValidator, CnpjValidator } from 'src/app/shared/validators';
 
 @Component({
   selector: 'app-cadastrar-pj',
@@ -32,7 +32,7 @@ export class CadastrarPjComponent implements OnInit {
       senha: ['', [Validators.required, Validators.minLength(6)]],
       cpf: ['', [Validators.required, CpfValidator]],
       razaoSocial: ['', [Validators.required, Validators.minLength(5)]],
-      cnpj: ['', [Validators.required]]
+      cnpj: ['', [Validators.required, CnpjValidator]]
     });
   }
 
